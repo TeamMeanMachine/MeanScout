6c0511049d05686d6b95edbe72cae29aa5a57bfd
Aidunlin <aidunlin@gmail.com>
2024-11-25 19:46:56 -0800

    Fix and improve service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -13,17 +13,17 @@ const CACHE_NAME = `MeanScout-${version}`;
 const UPLOAD_CACHE_NAME = `MS-Uploads-${version}`;
 const ASSETS = [...build, ...files, ...prerendered];
 
-sw.oninstall = (e) => e.waitUntil(install());
-sw.onactivate = (e) => e.waitUntil(activate());
-sw.onfetch = (e) => methods[e.request.method]?.(e);
+sw.oninstall = (e) => e.waitUntil(oninstall());
+sw.onactivate = (e) => e.waitUntil(onactivate());
+sw.onfetch = (e) => e.respondWith(onfetch(e));
 
-async function install() {
+async function oninstall() {
   await sw.skipWaiting();
   const cache = await caches.open(CACHE_NAME);
   await cache.addAll(ASSETS);
 }
 
-async function activate() {
+async function onactivate() {
   await sw.clients.claim();
   const keys = await caches.keys();
 
@@ -36,41 +36,48 @@ async function activate() {
   }
 }
 
-const methods = {
-  async GET(event: FetchEvent) {
-    const cache = await caches.open(CACHE_NAME);
+async function onfetch(event: FetchEvent) {
+  if (event.request.method == "POST") {
+    return await onpost(event);
+  }
 
-    try {
-      const response = await fetch(event.request);
+  return await onget(event);
+}
 
-      if (!(response instanceof Response)) {
-        throw new Error("Invalid response from fetch");
-      }
+async function onget(event: FetchEvent) {
+  const cache = await caches.open(CACHE_NAME);
 
-      if (response.status == 200) {
-        cache.put(event.request, response.clone());
-      }
+  try {
+    const networkResponse = await fetch(event.request);
 
-      return response;
-    } catch (error) {
-      const response = await cache.match(event.request);
-
-      if (!response) {
-        throw error;
-      }
-
-      return response;
-    }
-  },
-  async POST(event: FetchEvent) {
-    const formData = await event.request.formData();
-    const file = formData.get("file");
-
-    if (typeof file != "string") {
-      const cache = await caches.open(UPLOAD_CACHE_NAME);
-      await cache.put("upload", new Response(file));
+    if (!(networkResponse instanceof Response)) {
+      throw new Error("Invalid response from fetch");
     }
 
-    return Response.redirect("/", 303);
-  },
-};
+    if (networkResponse.status == 200) {
+      cache.put(event.request, networkResponse.clone());
+    }
+
+    return networkResponse;
+  } catch (error) {
+    const cachedResponse = await cache.match(event.request);
+
+    if (!cachedResponse) {
+      throw error;
+    }
+
+    return cachedResponse;
+  }
+}
+
+async function onpost(event: FetchEvent) {
+  const formData = await event.request.formData();
+  const file = formData.get("file");
+
+  if (typeof file != "string") {
+    const cache = await caches.open(UPLOAD_CACHE_NAME);
+    await cache.put("upload", new Response(file));
+  }
+
+  return Response.redirect("/", 303);
+}
