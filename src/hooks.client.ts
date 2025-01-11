98402202022636deabc386f39b5de7cac35fd173
Aidunlin <aidunlin@gmail.com>
2025-01-11 11:58:27 -0800

    Show settings page on first app launch

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,6 +2,10 @@ import { initIDB } from "$lib/idb";
 import type { ClientInit } from "@sveltejs/kit";
 
 export const init: ClientInit = async () => {
+  if (localStorage.length == 0) {
+    location.hash = `/settings`;
+  }
+
   if (localStorage.getItem("survey") && location.hash.replaceAll("/", "") == "") {
     location.hash = `/survey/${localStorage.getItem("survey")}`;
   }
