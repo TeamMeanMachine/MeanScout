05725380be9c969a3dae993b20ff192ed01e1007
Aidunlin <aidunlin@gmail.com>
2025-05-14 13:18:01 -0700

    Force full reload in dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    {
+      name: "full-reload-always",
+      handleHotUpdate({ server }) {
+        server.ws.send({ type: "full-reload" });
+        return [];
+      },
+    },
+  ],
 });
