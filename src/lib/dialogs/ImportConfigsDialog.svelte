d8ae8dc592cf0ebfc69e09de2084e1045a1eca60
Aidunlin <aidunlin@gmail.com>
2025-10-04 14:31:19 -0700

    Fix config import/merge

--- src/lib/dialogs/ImportConfigsDialog.svelte
+++ src/lib/dialogs/ImportConfigsDialog.svelte
@@ -5,7 +5,6 @@
   import Button from "$lib/components/Button.svelte";
   import QRCodeReader from "$lib/components/QRCodeReader.svelte";
   import { closeDialog, type DialogExports } from "$lib/dialog";
-  import type { Entry } from "$lib/entry";
   import type { Field } from "$lib/field";
   import { idb } from "$lib/idb";
   import { cameraStore } from "$lib/settings";
@@ -16,12 +15,10 @@
     compRecord,
     surveyRecords,
     fieldRecords,
-    entryRecords,
   }: {
     compRecord: Comp;
     surveyRecords: Survey[];
     fieldRecords: Field[];
-    entryRecords: Entry[];
   } = $props();
 
   const tab = sessionStorageStore<"qrfcode" | "file">("import-data-tab", $cameraStore ? "qrfcode" : "file");
@@ -30,38 +27,6 @@
   let importedComp = $state<Comp | undefined>();
   let importedSurveys = $state<Survey[] | undefined>();
   let importedFields = $state<Map<string, Field> | undefined>();
-
-  let compChanges = $state<{
-    name: boolean;
-    tbaEventKey: boolean;
-    matches: boolean;
-    matchesEdited: number;
-    teams: boolean;
-    teamsEdited: number;
-    scouts: boolean;
-  }>({
-    name: false,
-    tbaEventKey: false,
-    matches: false,
-    matchesEdited: 0,
-    teams: false,
-    teamsEdited: 0,
-    scouts: false,
-  });
-
-  let surveyChanges = $state<
-    Record<
-      string,
-      {
-        name: boolean;
-        type: boolean;
-        fields: boolean;
-        tbaMetrics: boolean;
-        pickLists: boolean;
-        expressions: boolean;
-      }
-    >
-  >({});
 
   let error = $state("");
 
@@ -149,14 +114,6 @@
     }
 
     if (jsonComp) {
-      if (jsonComp.name != compRecord.name) {
-        compChanges.name = true;
-      }
-
-      if (jsonComp.tbaEventKey != compRecord.tbaEventKey) {
-        compChanges.tbaEventKey = true;
-      }
-
       const matches = new Map<number, Match>();
 
       for (const match of compRecord.matches) {
@@ -192,12 +149,9 @@
               existingMatch.blueScore = importedMatch.blueScore;
             }
 
-            compChanges.matches = true;
-            compChanges.matchesEdited++;
             matches.set(importedMatch.number, existingMatch);
           }
         } else {
-          compChanges.matches = true;
           matches.set(importedMatch.number, importedMatch);
         }
       }
@@ -213,13 +167,10 @@
 
         if (existingTeam) {
           if (importedTeam.name != existingTeam.name) {
-            compChanges.teams = true;
-            compChanges.teamsEdited++;
             existingTeam.name = importedTeam.name;
             teams.set(importedTeam.number, existingTeam);
           }
         } else {
-          compChanges.teams = true;
           teams.set(importedTeam.number, importedTeam);
         }
       }
@@ -240,10 +191,6 @@
       };
 
       if (jsonComp.tbaEventKey || compRecord.tbaEventKey) {
-        if (jsonComp.tbaEventKey != compRecord.tbaEventKey) {
-          compChanges.tbaEventKey = true;
-        }
-
         comp.tbaEventKey = jsonComp.tbaEventKey || compRecord.tbaEventKey;
       }
 
@@ -254,10 +201,6 @@
       }
 
       for (const scout of jsonComp.scouts || []) {
-        if (!scouts.has(scout)) {
-          compChanges.scouts = true;
-        }
-
         scouts.add(scout);
       }
 
@@ -273,53 +216,21 @@
 
     if (jsonSurveys?.length) {
       for (const jsonSurvey of jsonSurveys) {
+        const surveyRecord = surveyRecords.find((s) => s.id == jsonSurvey.id);
+
         const jsonFields = json.fields?.filter((jsonField) => jsonField.surveyId == jsonSurvey.id);
 
-        if (
-          entryRecords.filter((e) => e.surveyId == jsonSurvey.id).length &&
-          (jsonFields?.length || 0) != fieldRecords.length
-        ) {
-          continue;
+        if (!importedFields) {
+          importedFields = new Map();
         }
-
-        const surveyRecord = surveyRecords.find((s) => s.id == jsonSurvey.id);
-        if (!surveyRecord) {
-          importedSurveys = [...(importedSurveys || []), jsonSurvey];
-          continue;
-        }
-
-        const changes: {
-          name: boolean;
-          type: boolean;
-          fields: boolean;
-          tbaMetrics: boolean;
-          pickLists: boolean;
-          expressions: boolean;
-        } = {
-          name: false,
-          type: false,
-          fields: false,
-          tbaMetrics: false,
-          pickLists: false,
-          expressions: false,
-        };
-
-        if (jsonSurvey.name != surveyRecord.name) {
-          changes.name = true;
-        }
-
-        if (jsonSurvey.type != surveyRecord.type) {
-          changes.type = true;
-        }
-
-        importedFields = new Map();
 
         for (const field of jsonFields || []) {
           importedFields.set(field.id, field);
         }
 
-        if (importedFields.size != fieldRecords.length) {
-          changes.fields = true;
+        if (!surveyRecord) {
+          importedSurveys = [...(importedSurveys || []), jsonSurvey];
+          continue;
         }
 
         if (jsonSurvey.type == "match") {
@@ -334,14 +245,6 @@
             created: surveyRecord.created,
             modified: surveyRecord.modified,
           };
-
-          if (surveyRecord.type == "match") {
-            changes.pickLists = jsonSurvey.pickLists.length != surveyRecord.pickLists.length;
-            changes.expressions = jsonSurvey.expressions.length != surveyRecord.expressions.length;
-          } else {
-            changes.pickLists = !!jsonSurvey.pickLists.length;
-            changes.expressions = !!jsonSurvey.expressions.length;
-          }
 
           if (jsonSurvey.tbaMetrics?.length) {
             importedSurvey.tbaMetrics = jsonSurvey.tbaMetrics;
@@ -374,18 +277,6 @@
     importedComp = undefined;
     importedSurveys = undefined;
     importedFields = undefined;
-
-    compChanges = {
-      name: false,
-      tbaEventKey: false,
-      matches: false,
-      matchesEdited: 0,
-      teams: false,
-      teamsEdited: 0,
-      scouts: false,
-    };
-
-    surveyChanges = {};
   }
 </script>
 
@@ -422,86 +313,56 @@
 {/if}
 
 {#if importedComp || importedSurveys?.length}
-  <div class="grid max-h-[500px] grid-cols-3 gap-1 overflow-auto">
-    <div class="text-sm font-light">Data</div>
-    <div class="text-sm font-light">Existing</div>
-    <div class="text-sm font-light">Imported</div>
+  <div
+    class="grid max-h-[500px] items-center gap-x-3 gap-y-1 overflow-auto text-sm"
+    style="grid-template-columns:min-content auto auto"
+  >
+    <div class="text-xs font-light">Data</div>
+    <div class="text-xs font-light">Existing</div>
+    <div class="text-xs font-light">Imported</div>
 
     {#if importedComp}
-      {#if compChanges.name}
-        <div>Name</div>
-        <div>{compRecord.name}</div>
-        <div>{importedComp.name}</div>
-      {/if}
+      <div class="text-base">Comp</div>
+      <div class="text-base">{compRecord.name}</div>
+      <div class="text-base">{importedComp.name}</div>
 
-      {#if compChanges.tbaEventKey}
-        <div>TBA Event Key</div>
-        <div>{compRecord.tbaEventKey || "-"}</div>
-        <div>{importedComp.tbaEventKey || "-"}</div>
-      {/if}
+      <div class="text-xs font-light text-nowrap">Event Key</div>
+      <div>{compRecord.tbaEventKey || "-"}</div>
+      <div>{importedComp.tbaEventKey || "-"}</div>
 
-      {#if compChanges.matches}
-        <div>Matches</div>
-        <div>{compRecord.matches.length || "-"}</div>
-        <div>
-          {importedComp.matches.length || "-"}
-          {#if compChanges.matchesEdited}
-            ({compChanges.matchesEdited} edited)
-          {/if}
-        </div>
-      {/if}
+      <div class="text-xs font-light">Matches</div>
+      <div>{compRecord.matches.length || "-"}</div>
+      <div>{importedComp.matches.length || "-"}</div>
 
-      {#if compChanges.teams}
-        <div>Teams</div>
-        <div>{compRecord.teams.length || "-"}</div>
-        <div>
-          {importedComp.teams.length || "-"}
-          {#if compChanges.teamsEdited}
-            ({compChanges.teamsEdited} edited)
-          {/if}
-        </div>
-      {/if}
+      <div class="text-xs font-light">Teams</div>
+      <div>{compRecord.teams.length || "-"}</div>
+      <div>{importedComp.teams.length || "-"}</div>
 
-      {#if importedComp.scouts?.length != compRecord.scouts?.length}
-        <div>Scouts</div>
-        <div>{compRecord.scouts?.length || "-"}</div>
-        <div>{importedComp.scouts?.length || "-"}</div>
-      {/if}
+      <div class="text-xs font-light">Scouts</div>
+      <div>{compRecord.scouts?.length || "-"}</div>
+      <div>{importedComp.scouts?.length || "-"}</div>
+
+      <div class="text-xs font-light">Fields</div>
+      <div>{fieldRecords.length || "-"}</div>
+      <div>{importedFields?.size || "-"}</div>
     {/if}
 
     {#each importedSurveys || [] as importedSurvey}
       {@const surveyRecord = surveyRecords.find((survey) => survey.id == importedSurvey.id)}
-      {@const changes = surveyChanges[importedSurvey.id]}
 
-      {#if surveyRecord && changes}
-        {#if changes.name}
-          <div>Name</div>
-          <div>{surveyRecord.name}</div>
-          <div>{importedSurvey.name}</div>
-        {/if}
+      {#if surveyRecord}
+        <div class="text-base">Survey</div>
+        <div class="text-base">{surveyRecord.name}</div>
+        <div class="text-base">{importedSurvey.name}</div>
 
-        {#if changes.type}
-          <div>Type</div>
-          <div>{surveyRecord.type}</div>
-          <div>{importedSurvey.type}</div>
-        {/if}
+        {#if surveyRecord.type == "match" && importedSurvey.type == "match"}
+          <div class="text-xs font-light">Pick Lists</div>
+          <div>{surveyRecord.pickLists.length}</div>
+          <div>{importedSurvey.pickLists.length}</div>
 
-        {#if changes.fields}
-          <div>Fields</div>
-          <div>{fieldRecords.length || "-"}</div>
-          <div>{importedFields?.size || "-"}</div>
-        {/if}
-
-        {#if changes.pickLists}
-          <div>Pick Lists</div>
-          <div>{(surveyRecord.type == "match" ? surveyRecord.pickLists.length : 0) || "-"}</div>
-          <div>{(importedSurvey.type == "match" ? importedSurvey.pickLists.length : 0) || "-"}</div>
-        {/if}
-
-        {#if changes.expressions}
-          <div>Expressions</div>
-          <div>{(surveyRecord.type == "match" ? surveyRecord.expressions.length : 0) || "-"}</div>
-          <div>{(importedSurvey.type == "match" ? importedSurvey.expressions.length : 0) || "-"}</div>
+          <div class="text-xs font-light">Expressions</div>
+          <div>{surveyRecord.expressions.length}</div>
+          <div>{importedSurvey.expressions.length}</div>
         {/if}
       {/if}
     {/each}
