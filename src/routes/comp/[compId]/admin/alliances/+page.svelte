0139feb7737d011782d424d80978f04fec9af5a9
Aidunlin <aidunlin@gmail.com>
2025-10-24 15:48:50 -0700

    Fix new alliance team buttons

--- src/routes/comp/[compId]/admin/alliances/+page.svelte
+++ src/routes/comp/[compId]/admin/alliances/+page.svelte
@@ -10,11 +10,12 @@
 
   let { data }: PageProps = $props();
 
+  let compRecord = $state($state.snapshot(data.compRecord));
+
   const standardCols = 5;
 
   const mostBackups = $derived(
-    (data.compRecord.alliances || []).map((a) => a.teams.length).reduce((prev, curr) => Math.max(prev, curr - 3), 0) ||
-      0,
+    (compRecord.alliances || []).map((a) => a.teams.length).reduce((prev, curr) => Math.max(prev, curr - 3), 0) || 0,
   );
 
   const teamLabels = ["Captain", "1st Pick", "2nd Pick"];
@@ -25,8 +26,8 @@
     class="-mx-3 -my-1 grid gap-3 overflow-x-auto px-3 py-1 text-center"
     style="grid-template-columns: repeat({mostBackups + standardCols}, min-content);"
   >
-    <div class="col-span-full grid grid-cols-subgrid text-xs font-light text-nowrap">
-      <div>Alliance</div>
+    <div class="col-span-full grid grid-cols-subgrid text-xs font-light tracking-tighter text-nowrap">
+      <div class="-mr-2">Alliance</div>
       {#each teamLabels as label}
         <div>{label}</div>
       {/each}
@@ -34,57 +35,58 @@
         <div>Backup</div>
       {/if}
     </div>
-    {#each data.compRecord.alliances || [] as alliance, allianceIndex}
+
+    {#each compRecord.alliances || [] as alliance, allianceIndex}
       <div class="col-span-full grid grid-cols-subgrid items-center">
-        <div>{allianceIndex + 1}</div>
+        <div class="-mr-2">{allianceIndex + 1}</div>
+
         {#each alliance.teams as team, teamIndex}
           <Button
             onclick={() => {
               openDialog(EditAllianceTeamDialog, {
-                teams: data.compRecord.teams,
+                teams: compRecord.teams,
                 allianceIndex,
                 team,
                 teamLabel: teamLabels[teamIndex] || "Backup",
                 onedit(newTeam) {
-                  if (data.compRecord.alliances == undefined) return;
-                  data.compRecord.alliances[allianceIndex].teams[teamIndex] = newTeam;
-                  data.compRecord.modified = new Date();
-                  idb.put("comps", $state.snapshot(data.compRecord)).onsuccess = invalidateAll;
+                  if (compRecord.alliances == undefined) return;
+                  compRecord.alliances[allianceIndex].teams[teamIndex] = newTeam;
+                  compRecord.modified = new Date();
+                  idb.put("comps", $state.snapshot(compRecord)).onsuccess = invalidateAll;
                 },
                 ondelete() {
-                  if (data.compRecord.alliances == undefined) return;
-                  data.compRecord.modified = new Date();
-
+                  if (compRecord.alliances == undefined) return;
                   if (alliance.teams.length <= 1) {
-                    data.compRecord.alliances = data.compRecord.alliances.filter((_, i) => i != allianceIndex);
+                    compRecord.alliances.splice(allianceIndex, 1);
                   } else {
-                    data.compRecord.alliances[allianceIndex].teams = data.compRecord.alliances[
-                      allianceIndex
-                    ].teams.filter((t) => t != team);
+                    compRecord.alliances[allianceIndex].teams.splice(teamIndex, 1);
                   }
-
-                  idb.put("comps", $state.snapshot(data.compRecord)).onsuccess = invalidateAll;
+                  compRecord.modified = new Date();
+                  idb.put("comps", $state.snapshot(compRecord)).onsuccess = invalidateAll;
                 },
               });
             }}
-            class="justify-center"
+            class="min-w-15 justify-center self-stretch px-1"
           >
             {team}
           </Button>
         {/each}
+
         {#each { length: mostBackups - (alliance.teams.length - 3) }}
           <div></div>
         {/each}
+
         <Button
           onclick={() => {
             openDialog(NewAllianceTeamDialog, {
-              teams: data.compRecord.teams,
+              teams: compRecord.teams,
               allianceIndex,
               teamLabel: teamLabels[alliance.teams.length] || "Backup",
               onadd(newTeam) {
-                data.compRecord.alliances = [...(data.compRecord.alliances || []), { teams: [newTeam] }];
-                data.compRecord.modified = new Date();
-                idb.put("comps", $state.snapshot(data.compRecord)).onsuccess = invalidateAll;
+                if (compRecord.alliances == undefined) return;
+                compRecord.alliances[allianceIndex].teams.push(newTeam);
+                compRecord.modified = new Date();
+                idb.put("comps", $state.snapshot(compRecord)).onsuccess = invalidateAll;
               },
             });
           }}
@@ -94,20 +96,25 @@
         </Button>
       </div>
     {/each}
+
     <Button
       onclick={() => {
         openDialog(NewAllianceTeamDialog, {
-          teams: data.compRecord.teams,
-          allianceIndex: (data.compRecord.alliances || []).length,
+          teams: compRecord.teams,
+          allianceIndex: (compRecord.alliances || []).length,
           teamLabel: teamLabels[0],
           onadd(newTeam) {
-            data.compRecord.alliances = [...(data.compRecord.alliances || []), { teams: [newTeam] }];
-            data.compRecord.modified = new Date();
-            idb.put("comps", $state.snapshot(data.compRecord)).onsuccess = invalidateAll;
+            if (compRecord.alliances == undefined) {
+              compRecord.alliances = [{ teams: [newTeam] }];
+            } else {
+              compRecord.alliances.push({ teams: [newTeam] });
+            }
+            compRecord.modified = new Date();
+            idb.put("comps", $state.snapshot(compRecord)).onsuccess = invalidateAll;
           },
         });
       }}
-      class="col-span-1 justify-self-center"
+      class="col-span-1 -mr-2 justify-self-center"
     >
       <PlusIcon class="text-theme" />
     </Button>
