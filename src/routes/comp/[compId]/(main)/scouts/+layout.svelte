36305a0a7019af56d958b5081cfc25ca58982bcb
Aidunlin <aidunlin@gmail.com>
2026-06-08 17:44:56 -0700

    Condense scouts list

--- src/routes/comp/[compId]/(main)/scouts/+layout.svelte
+++ src/routes/comp/[compId]/(main)/scouts/+layout.svelte
@@ -7,66 +7,60 @@
 
 <div
   class={[
-    "w-120 lg:fixed lg:top-14.25 lg:h-[calc(100vh-57px)] lg:overflow-y-auto lg:overscroll-y-contain lg:border-r lg:border-neutral-600",
-    "max-lg:mx-auto max-lg:w-full max-lg:max-w-(--breakpoint-lg)",
+    "lg:fixed lg:top-14.25 lg:h-[calc(100vh-57px)] lg:w-100 lg:overflow-y-auto lg:overscroll-y-contain lg:border-r lg:border-neutral-600",
     data.scoutName ? "max-lg:hidden" : "max-lg:mb-16.25",
   ]}
 >
-  <div class={["flex flex-col gap-3 bg-neutral-900 px-3 py-6", "sticky top-14.25 z-20 lg:top-0", "max-lg:mt-14.25"]}>
-    <h2 class="font-bold">Scouts</h2>
-  </div>
-
-  {#if data.predictionsPerScout.length}
-    <div class={[data.scoutName ? "" : "max-lg:px-3"]}>
-      <div
-        class={["mb-3 grid gap-x-3 gap-y-2 px-3 max-lg:-mx-3 max-lg:overflow-x-auto max-lg:px-3"]}
-        style="grid-template-columns: auto repeat(5, min-content);"
-      >
-        <div class="col-span-full grid grid-cols-subgrid items-end gap-x-4 px-2 text-center text-xs tracking-tighter">
-          <div class="text-left">Scout</div>
-          <div>Adjusted</div>
-          <div>Total</div>
-          <div class="text-nowrap">Co-op</div>
-          <div>Correct</div>
-          <div>Accuracy</div>
+  {#if !data.predictionsPerScout.length}
+    <div class="mx-3 my-6 text-xs">No guesses.</div>
+  {:else}
+    <div
+      class="grid gap-x-3 gap-y-2 p-3 pt-4.5 pb-6 max-lg:mt-14.25 max-lg:overflow-x-auto"
+      style="grid-template-columns: auto repeat(3, min-content);"
+    >
+      <div class="col-span-full grid grid-cols-subgrid items-center px-2 text-center font-light tracking-tighter">
+        <div class="-ml-2 text-left text-base font-bold tracking-normal">Scouts</div>
+        <div>
+          <div class="text-sm font-normal tracking-normal">Adjusted</div>
+          <div class="text-xs">Total</div>
         </div>
-
-        {#each data.predictionsPerScout as { scout, entries, points, coopPoints, correctGuesses, accuracy, adjustedPoints }}
-          {@const viewing = scout.name == data.scoutName && scout.team == data.scoutTeam}
-          {@const urlParams = scout.team ? `?team=${encodeURIComponent(scout.team)}` : ""}
-
-          <Anchor
-            route="comp/{data.compRecord.id}/scouts/{encodeURIComponent(scout.name)}{urlParams}"
-            class="col-span-full grid grid-cols-subgrid {viewing ? 'font-bold' : ''}"
-          >
-            <div class="flex min-w-24 flex-col truncate">
-              <span class={viewing ? "underline" : ""}>{scout.name}</span>
-              {#if scout.team}
-                <span class="text-xs font-light">{scout.team}</span>
-              {/if}
-            </div>
-            <div class="text-center">{adjustedPoints.toFixed(2)}</div>
-            <div class="text-center">{points}</div>
-            <div class="text-center">{coopPoints}</div>
-            <div class="text-center">{correctGuesses}<span class="text-xs font-light">/{entries.length}</span></div>
-            <div class="text-center">{(accuracy * 100).toFixed(1)}<span class="text-xs font-light">%</span></div>
-          </Anchor>
-        {/each}
-
-        <div class="col-span-full grid grid-cols-subgrid p-2 text-center text-sm">
-          <div class="text-left">Overall</div>
-          <div>{data.totalAdjustedPoints.toFixed(2)}</div>
-          <div>{data.totalPoints}</div>
-          <div>{data.totalCoopPoints}</div>
-          <div>
+        <div class="text-xs text-nowrap">Co-op</div>
+        <div class="m">
+          <div class="text-sm font-normal tracking-normal">
             {data.totalCorrectGuesses}<span class="text-xs font-light">/{data.entriesWithPredictions.length}</span>
           </div>
-          <div>{(data.overallAccuracy * 100).toFixed(1)}<span class="text-xs font-light">%</span></div>
+          <div class="text-xs font-normal tracking-normal">
+            {(data.overallAccuracy * 100).toFixed(1)}<span class="text-xs font-light">%</span>
+          </div>
         </div>
       </div>
+
+      {#each data.predictionsPerScout as { scout, entries, points, coopPoints, correctGuesses, accuracy, adjustedPoints }}
+        {@const viewing = scout.name == data.scoutName && scout.team == data.scoutTeam}
+        {@const urlParams = scout.team ? `?team=${encodeURIComponent(scout.team)}` : ""}
+
+        <Anchor
+          route="comp/{data.compRecord.id}/scouts/{encodeURIComponent(scout.name)}{urlParams}"
+          class="col-span-full grid grid-cols-subgrid {viewing ? 'font-bold' : ''}"
+        >
+          <div class="flex min-w-24 flex-col truncate">
+            <div class={["truncate", viewing && "underline"]}>{scout.name}</div>
+            {#if scout.team}
+              <span class="text-xs font-light">{scout.team}</span>
+            {/if}
+          </div>
+          <div class="text-center">
+            {adjustedPoints.toFixed(2)}
+            <div class="text-xs font-light">{points}</div>
+          </div>
+          <div class="text-center text-xs">{coopPoints}</div>
+          <div class="text-center">
+            {correctGuesses}<span class="text-xs font-light">/{entries.length}</span>
+            <div class="text-xs font-light">{(accuracy * 100).toFixed(1)}%</div>
+          </div>
+        </Anchor>
+      {/each}
     </div>
-  {:else}
-    <span>No guesses.</span>
   {/if}
 </div>
 
